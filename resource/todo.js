c5fbeaf30e9aa43824c91b2680d836f5a1b52287
Nista-Shakya <nista.shakya@gmail.com>
2022-10-12 20:50:21 +0545

    functionable add,edit and delete added

---
+++ resource/todo.js
@@ -0,0 +1,28 @@
+const todos = [
+    {
+        "_id": "001",
+        "title": "Complete bootcamp project",
+        "description": "Talk to team, make team repository, assign task",
+    },
+
+    {
+        "_id": "002",
+        "title": "Complete bootcamp project 2",
+        "description": "Talk to team, make team repository, assign task 2",
+    },
+
+    {
+        "_id": "003",
+        "title": "Complete bootcamp project 3",
+        "description": "Talk to team, make team repository, assign task 3",
+    },
+
+    {
+        "_id": "004",
+        "title": "Complete bootcamp project 4",
+        "description": "Talk to team, make team repository, assign task 4",
+    }
+
+]
+
+module.exports = todos;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,37 @@
 var express = require('express');
+var todos = require('../resource/todo')
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nista' });
+  res.render('home', {todosList:todos});
 });
 
-router.get('/home', function(req, res, next) {
-  res.render('home');
+router.get('/add-to-do', function(req, res, next) {
+  res.render('addTodo', {title: 'Add To Do  '});
+});
+
+router.post('/save-to-do', function(req, res, next) {
+  todos.push({...req.body, _id: '00${todos.length}'}); //concatination
+  res.redirect('/');
+});
+
+router.get('/delete-to-do/:index', function(req, res, next) {
+  console.log(req.params.index);
+  todos.splice(req.params.index, 1); 
+  res.redirect('/');
+});
+
+router.get('/open-update-form/:id', function(req, res, next){
+  const todotodo = todos.find(todo => todo._id === req.params.id)
+  res.render('editToDo', {todo: todotodo});
+});
+
+router.post('/update-to-do/:id', function(req, res, next){
+  const index = todos.findIndex(todo => todo._id === req.params.id);
+  todos.splice(index,1, {...req.body, _id: req.params.id});
+  res.redirect('/');
 });
 
 module.exports = router;
